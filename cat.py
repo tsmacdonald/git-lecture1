7827860d06f4f33253b266bbe274cf753cc39ea3
Timothy Macdonald <tmacdonald@groupon.com>
2014-02-04 19:27:16 -0600

    Add totally-useful Cat class

--- cat.py
+++ cat.py
@@ -1,8 +1,26 @@
 class Cat():
-  def __init__(self, files):
+  def __init__(self, name, files):
     self.files = files
+    self.name = name
+
   def run(self):
     for file in self.files:
       with open(file) as f:
         for line in f:
           print line,
+
+  def greet():
+    print """
+   /\     /\\
+  {  `---'  }
+  {  O   O  }
+  ~~>  V  <~~   Hello, %s!
+   \  \|/  /
+    `-----'__
+    /     \  `^\_
+   {       }\ |\_\_   W
+   |  \_/  |/ /  \_\_( )
+    \__/  /(_E     \__/
+      (  /
+       MM
+       """%self.name
